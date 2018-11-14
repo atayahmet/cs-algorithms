a158cadc4750464fe795f5cd7c5d3ffa0103b8ce
Ahmet ATAY <ahmet@efabrika.com>
2018-11-15 00:25:22 +0300

    removed dist

--- hashtable/dist/services/linkedlist/node.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Node = /** @class */ (function () {
-    function Node(data) {
-        this.data = data;
-    }
-    return Node;
-}());
-exports["default"] = Node;
